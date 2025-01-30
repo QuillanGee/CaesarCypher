ba17776144cb725b1d93a98e36f3881d984eb0e4
Quillan Gee <qgee@chapman.edu>
2025-01-29 20:18:36 -0800

    Graedon's changes

--- CaesarCypherCode/Program.cs
+++ CaesarCypherCode/Program.cs
@@ -4,7 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
         // CaesarCypher.Encode("Zob", 3);
         string answer = CaesarCypher.Crack("Xli ibxirhih evq wepyxmrk kiwxyvi aew eppikih xs fi fewih sr er ergmirx Vsqer " +
                            "gywxsq, fyx rs orsar Vsqer asvo sj evx hitmgxw mx, rsv hsiw erc ibxerx Vsqer xibx " +
@@ -12,8 +11,5 @@ class Program
                            "Neguyiw-Psymw Hezmh'w 1784 temrxmrk Sexl sj xli Lsvexmm, almgl hmwtpecih e" +
                            "vemwih evq wepyxexsvc kiwxyvi mr er ergmirx Vsqer wixxmrk. Xli kiwxyvi erh mxw" +
                            "mhirmjmgexmsr amxl ergmirx Vsqi aew ehzergih mr sxliv Jvirgl risgpewwmg evx.");
-        string answer2 =
-            CaesarCypher.Crack(
-                "Bpx fivvwi lezi mwx gsqtpixr xmppmrk! Lsa xlmw hi sj wxs qegmgxmrk sj xli wmziivmrk! Hievsyx lezi mr wxs mwx xlmw, tpevi xlmw mwx qmqip.");
     }
 }
--- CaesarCypherCode/obj/Debug/net8.0/CaesarCypherCode.AssemblyInfo.cs
+++ CaesarCypherCode/obj/Debug/net8.0/CaesarCypherCode.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CaesarCypherCode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8f8bcefb450b83eeaceb2f47e8d1a35b5ce1d6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CaesarCypherCode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CaesarCypherCode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
